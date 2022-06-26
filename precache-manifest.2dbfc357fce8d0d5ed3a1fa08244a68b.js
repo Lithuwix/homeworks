e29bf1773c715915483e11b615a2db41f0aea6b2
TokiLithuwix <loremlisumdol@gmail.com>
2022-06-27 06:34:26 +0700

    Updates

---
+++ precache-manifest.2dbfc357fce8d0d5ed3a1fa08244a68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e0b7bb43b4ef97fecf515ff328ea44",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "a305c723c922656ff2a2",
+    "url": "/homeworks/static/css/main.6cd49a54.chunk.css"
+  },
+  {
+    "revision": "ddb07063c25ebc850cda",
+    "url": "/homeworks/static/js/2.485c17d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/homeworks/static/js/2.485c17d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a305c723c922656ff2a2",
+    "url": "/homeworks/static/js/main.8ca8ce0e.chunk.js"
+  },
+  {
+    "revision": "4973bd5b858cd224b7ef",
+    "url": "/homeworks/static/js/runtime-main.047caa20.js"
+  }
+]);
